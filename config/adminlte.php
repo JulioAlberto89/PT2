46fe4c5240c46d9127abcbcd2b68712d73f2633a
JulioAlberto89 <jamv19.06.89@gmail.com>
2024-07-08 08:21:37 +0200

    Limita el acceso al usuario no admin.

--- config/adminlte.php
+++ config/adminlte.php
@@ -313,7 +313,7 @@ return [
         [
             'text' => 'blog',
             'url' => 'admin/blog',
-            'can' => 'manage-blog',
+            'can' => 'isAdmin',
         ],
         // [
         //     'text' => 'pages',
